2d02d72972fa8484c0c112201ffbf3984310300c
Jérôme L'Allier-Décary <lallierdecary.j@hotmail.com>
2022-02-28 01:00:46 -0500

    Add Solution, project and base elements

---
+++ CrudRepo/Interfaces/IRepository.cs
@@ -0,0 +1,11 @@
+﻿using CrudRepo.Models;
+
+namespace CrudRepo.Interfaces;
+
+public interface IRepository<T> where T : CrudBaseElement
+{
+    Task<T> CreateAsync(T entity);
+    Task<T> ReadAsync(T entity);
+    Task<T> UpdateAsync(T entity); 
+    Task DeleteAsync(T entity);
+}
---
+++ CrudRepo/Models/CrudBaseElement.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrudRepo.Models;
+
+public class CrudBaseElement
+{
+    public string Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
